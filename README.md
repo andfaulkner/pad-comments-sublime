850d349b1c709200d2817ce894b739e0a476aea8
mborgerson <mborgerson@gmail.com>
2013-01-18 02:26:38 -0700

    Supports alignment, custom fill characters, and multiple cursors.

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
 Pad
 ===
 
-Sublime Text 2 Package to Add Padding to Lines
+Pad is a [Sublime Text 2](http://www.sublimetext.com/) plug-in to add custom
+padding to lines.
+
+_Note: This is a new package and is subject to minor changes in behavior._
+
+Installation
+------------
+
+Install by cloning this repository to your Packages directory.
+
+* For Windows:
+
+        cd "%APPDATA%\Sublime Text 2\Packages"
+        git clone https://github.com/mborgerson/pad.git
+
+* For Mac OS X:
+
+        cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/
+        git clone https://github.com/mborgerson/pad.git
+
+After cloning the repository to your packages directory, Sublime Text should
+automatically load the package.
+
+How to Use
+----------
+
+First, move the cursor to the line you want to pad or select the region of text
+in the line that should be padded. You may use multiple cursors to pad multiple
+lines at once. Then, use the command palette (`Ctrl+Shift+P`) to find and run
+the command with the caption "Pad: Add Padding to Line or Selection". You will
+be prompted for alignment and fill character. The line/selected text will be
+padded with the fill character up to the first ruler (or column 80 if no rulers
+are being used).
+
+Examples
+--------
+
+* Use no selection to pad the entire line.
+
+        This is some text.
+
+ becomes
+
+        --------------------------This is some text.--------------------------
+
+* Use multiple selections to batch pad.
+
+        /* This is some text. */
+		/* This is some more text. */
+		/* And here is even more text. */
+
+ becomes
+
+        /* -----------------------This is some text.----------------------- */
+		/* --------------------This is some more text.--------------------- */
+		/* ------------------And here is even more text.------------------- */
+
+* Perform the command on an empty line to fill the entire line with a character.
+
+ ` `
+
+ becomes
+
+        **********************************************************************
+
+Limitations
+-----------
+* Padding only works for one region of text per line.
+* Multi-line selection padding does not work properly.
+  * Workaround: Use multiple cursors / multiple single-line selections to pad
+    multiple lines at once.
---
+++ pad.py
@@ -0,0 +1,100 @@
+"""
+Pad: A Sublime Text 2 plug-in to add custom padding to lines.
+Matthew Borgerson <mborgerson@gmail.com>
+"""
+
+import sublime, sublime_plugin
+
+Alignment = {
+    'left':   0,
+    'center': 1,
+    'right':  2
+}
+
+class PromptPadCommand(sublime_plugin.WindowCommand):
+    """
+    Prompts the user for the alignment and fill character, then runs the
+    pad command.
+    """
+    def run(self):
+        """Called when the command is run."""
+        self._state = 0
+        self._command_args = {
+            'fill_char': '-' # Default fill character
+            }
+        self._prompt_control()
+
+    def _prompt_control(self):
+        """A simple state machine to control the order of prompts."""
+        if self._state == 0:
+            # Prompt for alignment
+            keys = ['%s Aligned' % k.capitalize() for k in Alignment]
+            self.window.show_quick_panel(keys, self._on_prompt_align_done)
+        elif self._state == 1:
+            # Prompt for fill character
+            self.window.show_input_panel('Fill Character:',
+                                         '-',
+                                         self._on_prompt_fill_char_done,
+                                         None,
+                                         None)
+        elif self._state == 2:
+            # Perform padding
+            view = self.window.active_view()
+            if view is not None:
+                view.run_command('pad', self._command_args)
+
+    def _on_prompt_align_done(self, index):
+        """Called after the user has selected the text alignment."""
+        if index < 0: return
+        self._command_args['align'] = Alignment[Alignment.keys()[index]]
+        self._state += 1
+        self._prompt_control()
+
+    def _on_prompt_fill_char_done(self, text):
+        """Called after the user has entered the fill character."""
+        if text != '':
+            self._command_args['fill_char'] = text[0]
+        self._state += 1
+        self._prompt_control()
+
+class PadCommand(sublime_plugin.TextCommand):
+    """
+    Pad the selected text (or entire line) with a fill character up to the
+    first ruler (or column 80 if no rulers are being used).
+    """
+
+    DEFAULT_WIDTH = 80
+
+    def run(self, edit, fill_char='-', align=Alignment['center'], width=None):
+        """Called when the command is run."""
+        # Determine total pad width.
+        if width is None:
+            try:
+                # Use the first ruler if available.
+                width = int(self.view.settings().get('rulers')[0])
+            except IndexError:
+                width = self.DEFAULT_WIDTH
+
+        # Determine the alignment character to be used in the format string.
+        if   align == Alignment['left']:   align_char = '<'
+        elif align == Alignment['center']: align_char = '^'
+        elif align == Alignment['right']:  align_char = '>'
+
+        # Decorate each region.
+        for region in self.view.sel():
+            line = self.view.line(region)
+            if region.empty():
+                # No text selected, pad entire line contents.
+                contents       = self.view.substr(line)
+                format_width   = width
+                replace_region = line
+            else:
+                # Pad inside selected region.
+                contents       = self.view.substr(region)
+                format_width   = width - line.size() + region.size()
+                replace_region = region
+
+            # Generate the final line contents and insert it.
+            format_str = '{0:%s%s%d}' % (fill_char, align_char, format_width)
+            formatted_str = format_str.format(contents or fill_char)
+            self.view.replace(edit, replace_region, formatted_str)
